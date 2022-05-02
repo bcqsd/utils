f8a38c1534a12b70f39ddcecbedfa37062fb0625
bcqsd <20204248@cqu.edu.cn>
2022-05-02 22:58:56 +0800

    finish maogai task

--- test.js
+++ test.js
@@ -1,7 +1,18 @@
-export function f(){
-    console.log(1);
+function process(){
+    let data=[]
+    const options=['A','B','C','D']
+    for(let i=0;i<32;++i){
+        data[i]=new Array(30).fill(0)
+        data[i][0]=i+3
+        data[i][1]=data[i][2]='重庆市'
+        for(let j=5;j<30;++j){
+            let rand=Math.floor((Math.random()*4))
+            data[i][j]=options[rand]
+        } 
+        data[i][3]='B'
+        data[i][4]=Math.random()<0.5?'A':'B'
+    }
+    console.log(data)
 }
-let arr=[1]
-export {
-    arr
-}
+
+process()
--- xls/processSheet.js
+++ xls/processSheet.js
@@ -1,10 +1,37 @@
 import * as XLSX from 'xlsx/xlsx.mjs';
 
 export function processSheet(worksheet){
-    XLSX.utils.sheet_add_aoa(worksheet, [
-        [3],                             // <-- Write 1 to cell B3
-        ,                                // <-- Do nothing in row 4
-        [/*B5*/, /*C5*/, /*D5*/, "abc"]  // <-- Write "abc" to cell E5
-      ], { origin: "B3" });
-    
+    process(worksheet)
+}
+
+
+
+
+function process(sheet){
+    let data=[]
+    const option4=['A','B','C','D']
+    const option3=['A','B','C']
+    let have3=[4,5,12,13,14,15,21,22]
+    have3=have3.map(p=>p+2)
+    for(let i=0;i<32;++i){
+        data[i]=new Array(30).fill(0)
+        data[i][0]=i+3
+        data[i][1]=data[i][2]='重庆市'
+        for(let j=5;j<30;++j){
+            let rand4=~~(Math.random()*4)
+            let rand3=~~(Math.random()*3)
+            data[i][j]=have3.indexOf(j)!=-1?option3[rand3]:option4[rand4]
+        } 
+        data[i][3]='B'
+        data[i][4]=Math.random()<0.5?'A':'B'
+        data[i][19]=Math.random()<0.5?'A':'B'
+        data[i][25]=Math.floor(Math.random()*10)
+        data[i][26]=Math.floor(Math.random()*10)
+        data[i][27]=['A','B','C','D','E','F','G','H'].filter(p=>Math.random()<0.5).join('')
+        data[i][28]=data[i][29]=''
+        if(data[i][19]!='A') data[i][20]=''
+        if(data[i][19]!='A') data[i][21]=''
+        data[i][22]=''
+    }
+    XLSX.utils.sheet_add_aoa(sheet, data, { origin: "A3" });
 }
